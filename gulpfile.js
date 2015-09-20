24b0e8a42988f528b734334fb8886c8e54d7b3e7
Alex Budaev <budaevaa@mail.ru>
2015-09-20 22:48:31 +0700

    add task sourcemaps but hi not work

--- gulpfile.js
+++ gulpfile.js
@@ -210,10 +210,19 @@ gulp.task('default', ['sass','browser-sync','watch']);
 gulp.task('build', function(callback) {
     runSequence('clean',
         'sass', ['images', 'copy', 'fonts', 'jquery'],
-        'html',
+        'html','sourcemaps',
         callback);
 });
 
+//под вопросом. Разобраться как работает
+gulp.task('sourcemaps', function(){
+    gulp.src('./dist/styles/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist.css))
+        .pipe(size({title: 'sourcemaps'}));
+
+});
 
 // Run PageSpeed Insights
 gulp.task('pagespeed', function (cb) {
